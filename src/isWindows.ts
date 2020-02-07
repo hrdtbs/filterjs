74772ca2c233f9a81ddf87dd62417954f267230c
hrdtbs <harada.tsubasa.0422@gmail.com>
2020-02-07 17:06:58 +0900

    fix: add support of firefox

--- src/isWindows.ts
+++ src/isWindows.ts
@@ -4,6 +4,7 @@ export const isWindows = () => {
         navigator.platform === "Win64" ||
         navigator.userAgent.indexOf("WOW64") != -1 ||
         navigator.userAgent.indexOf("Win64") != -1 ||
-        navigator.appVersion.indexOf("Win") != -1
+        navigator.appVersion.indexOf("Win") != -1 ||
+        (navigator.oscpu && (navigator.oscpu.indexOf("Win32") != -1 || navigator.oscpu.indexOf("Win64") != -1))
     )
 }
